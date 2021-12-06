84d6bce8548ed4b47974d7736c29fb1b669cdc72
ousttrue <ousttrue@gmail.com>
2021-12-07 02:18:26 +0900

    jsontree

--- src/gltfloupe/app.py
+++ src/gltfloupe/app.py
@@ -1,10 +1,15 @@
 import sys
 import pathlib
 import logging
+from typing import Optional, Union, Any
 import OpenGL.GL as gl
 import glfw
 import imgui
 from imgui.integrations.glfw import GlfwRenderer
+
+from gltfio.parser import GltfData
+
+
 logger = logging.getLogger(__name__)
 
 
@@ -79,16 +84,84 @@ class GlfwWindow:
         self.io = imgui.get_io()
         self.io.config_flags |= imgui.CONFIG_DOCKING_ENABLE
         self.impl = GlfwRenderer(self.window)
-        self.show_custom_window = True
+        self.show_json_tree = True
 
         # gl
         import glglue.gl3.samplecontroller
         self.controller = glglue.gl3.samplecontroller.SampleController()
+        self.gltf: Optional[GltfData] = None
+        self.selected = None
 
     def __del__(self):
         del self.controller
         self.impl.shutdown()
         glfw.terminate()
+
+    def _jsontree(self):
+        if not self.show_json_tree:
+            return
+        is_expand, self.show_json_tree = imgui.begin("jsontree", True)
+        flags = (
+            imgui.TABLE_BORDERS_VERTICAL
+            | imgui.TABLE_BORDERS_OUTER_HORIZONTAL
+            | imgui.TABLE_RESIZABLE
+            | imgui.TABLE_ROW_BACKGROUND
+            | imgui.TABLE_NO_BORDERS_IN_BODY
+        )
+        if is_expand:
+            if self.gltf:
+                if imgui.begin_table("jsontree_table", 2, flags):
+                    # header
+                    imgui.table_setup_column("key")
+                    imgui.table_setup_column("value")
+                    imgui.table_headers_row()
+
+                    # body
+                    def traverse(key: str, node: Union[list, dict, Any]):
+                        flag = 0  # const.ImGuiTreeNodeFlags_.SpanFullWidth
+                        match node:
+                            case list():
+                                value = f'({len(node)})'
+                            case dict():
+                                value = node.get('name', '')
+                            case _:
+                                flag |= imgui.TREE_NODE_LEAF
+                                flag |= imgui.TREE_NODE_BULLET
+                                # flag |= imgui.TREE_NODE_NO_TREE_PUSH_ON_OPEN
+                                value = f'{node}'
+                        imgui.table_next_row()
+                        # col 0
+                        imgui.table_next_column()
+                        open = imgui.tree_node(key, flag)
+                        imgui.set_item_allow_overlap()
+                        # col 1
+                        imgui.table_next_column()
+                        if node == self.selected:
+                            pass
+                        _, selected = imgui.selectable(
+                            value, node == self.selected, imgui.SELECTABLE_SPAN_ALL_COLUMNS)
+                        if selected:
+                            # update selctable
+                            self.selected = node
+                        if imgui.is_item_clicked():
+                            # update selctable
+                            self.selected = node
+                        if open:
+                            match node:
+                                case list():
+                                    for i, v in enumerate(node):
+                                        traverse(f'{i}', v)
+                                case dict():
+                                    for k, v in node.items():
+                                        traverse(k, v)
+                            imgui.tree_pop()
+
+                    imgui.set_next_item_open(True, imgui.ONCE)
+                    for k, v in self.gltf.gltf.items():
+                        traverse(k, v)
+
+                    imgui.end_table()
+        imgui.end()
 
     def _update(self):
         docking_space('docking_space')
@@ -104,17 +177,17 @@ class GlfwWindow:
                     exit(1)
 
                 imgui.end_menu()
+
+            if imgui.begin_menu("View", True):
+                _, self.show_json_tree = imgui.menu_item(
+                    "jsontree", '', self.show_json_tree, True
+                )
+
+                imgui.end_menu()
+
             imgui.end_main_menu_bar()
 
-        if self.show_custom_window:
-            is_expand, self.show_custom_window = imgui.begin(
-                "Custom window", True)
-            if is_expand:
-                imgui.text("Bar")
-                imgui.text_ansi("B\033[31marA\033[mnsi ")
-                imgui.text_ansi_colored("Eg\033[31mgAn\033[msi ", 0.2, 1., 0.)
-                imgui.extra.text_ansi_colored("Eggs", 0.2, 1., 0.)
-            imgui.end()
+        self._jsontree()
 
     def new_frame(self) -> bool:
         if glfw.window_should_close(self.window):
