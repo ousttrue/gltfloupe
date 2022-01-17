60ffa5c4116d516b4be901f84024e4df276137a9
ousttrue <ousttrue@gmail.com>
2022-01-17 14:42:23 +0900

    weight sum

--- src/gltfloupe/gui/accessor_table.py
+++ src/gltfloupe/gui/accessor_table.py
@@ -4,6 +4,12 @@ from gltfio.types import GltfAccessorSlice
 import pydear as imgui
 from ..jsonutil import get_value
 
+THRESHOLD = 1e-5
+
+
+def color_32(r, g, b, a):
+    return r + (g << 8) + (b << 16) + (a << 24)
+
 
 def get_accessor(data: GltfData, keys: tuple) -> Optional[int]:
     match keys:
@@ -26,6 +32,7 @@ class AccessorTable:
         self.view = view
 
     def draw(self):
+        imgui.TextUnformatted(f'count: {self.view.get_count()}')
         flags = (
             imgui.ImGuiTableFlags_.BordersV
             | imgui.ImGuiTableFlags_.BordersOuterH
@@ -33,12 +40,17 @@ class AccessorTable:
             | imgui.ImGuiTableFlags_.RowBg
             | imgui.ImGuiTableFlags_.NoBordersInBody
         )
-        if imgui.BeginTable("jsontree_table", self.view.element_count+1, flags):
+        cols = self.view.element_count+1
+        if self.key[-1] == 'WEIGHTS_0':
+            cols += 1
+        if imgui.BeginTable("jsontree_table", cols, flags):
             # header
             # imgui.TableSetupScrollFreeze(0, 1); // Make top row always visible
             imgui.TableSetupColumn('index')
             for i in range(self.view.element_count):
                 imgui.TableSetupColumn(f'{i}')
+            if self.key[-1] == 'WEIGHTS_0':
+                imgui.TableSetupColumn(f'sum')
             imgui.TableHeadersRow()
 
             # body
@@ -53,9 +65,27 @@ class AccessorTable:
                 imgui.TableNextColumn()
                 imgui.TextUnformatted(f'{i:05}')
                 #
+                total = 0
                 for j in range(self.view.element_count):
                     imgui.TableNextColumn()
-                    imgui.TextUnformatted(f'{next(it):.3f}')
+                    value = next(it)
+                    total += value
+                    imgui.TextUnformatted(f'{value:.3f}')
+                if self.key[-1] == 'WEIGHTS_0':
+                    imgui.TableNextColumn()
+
+                    d = total-1
+                    if d > THRESHOLD:
+                        imgui.PushStyleColor(
+                            imgui.ImGuiCol_.Text, color_32(255, 0, 0, 255))
+                    elif d < -THRESHOLD:
+                        imgui.PushStyleColor(
+                            imgui.ImGuiCol_.Text, color_32(0, 0, 255, 255))
+                    else:
+                        imgui.PushStyleColor(
+                            imgui.ImGuiCol_.Text, color_32(128, 128, 128, 255))
+                    imgui.TextUnformatted(f'{total:.3f}')
+                    imgui.PopStyleColor()
                 i += 1
 
             imgui.EndTable()
